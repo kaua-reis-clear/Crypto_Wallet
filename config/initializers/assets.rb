5b46dbbf33eb3f853bf9e63f59bc15120e122796
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-04-16 23:39:24 -0300

    using yarn

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,9 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 Rails.application.config.assets.precompile += %w( cable.js
                                                   coins.js
-                                                  jquery.js
                                                   mining_types.js
-                                                  notify.js
                                                   welcome.js
                                                 )
 
